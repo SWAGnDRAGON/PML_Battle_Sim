b383166ad4778ecf87a1ee943044fea5ab77c2d6
Chris Rickabaugh <ChrisRick98@gmail.com>
2026-03-18 16:39:33 -0400

    Player Animator and Input System Setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,37 @@
+using UnityEditor.Animations;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Animator Player_Anim;
+    public bool pause = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(InputSystem.actions["Attack"].WasPressedThisFrame())
+        {
+            Player_Anim.SetBool("IsDashing", !Player_Anim.GetBool("IsDashing"));
+        }
+
+        if (InputSystem.actions["Interact"].WasPressedThisFrame())
+        {
+            Player_Anim.SetBool("IsAttacking", !Player_Anim.GetBool("IsAttacking"));
+        }
+
+        if (InputSystem.actions["Jump"].WasPressedThisFrame())
+        {
+            pause = !pause;
+            if(pause)
+            {
+                Player_Anim.speed = 0f;
+            }
+            else
+            {
+                Player_Anim.speed = 1f;
+            }
+        }
     }
 }
