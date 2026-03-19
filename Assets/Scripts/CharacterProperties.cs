bbc585f86e6bdee78d7f1af3bd55a42b0e89cd36
Drayoia <xxdray64@gmail.com>
2026-03-19 15:49:46 -0400

    PR

---
+++ Assets/Scripts/CharacterProperties.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public abstract class CharacterProperties : MonoBehaviour
+{
+    public string characterName;
+    public int maxHealth;
+    public int currentHealth;
+    public int attack;
+    public int defense;
+}
