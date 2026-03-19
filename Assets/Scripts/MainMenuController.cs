64c15ec896dbdbed4fa24c447b0bd2a2c723f38b
Victor Phillips <victor.m.phillips@protonmail.com>
2026-03-18 21:07:02 -0400

    Main menu work (#5)
    * Main menu work
    
    Changes:
    Color / fonts changed
    buttons changed to have rounded corners via 9-splice sprites
    All around feels slightly less like a default Unity menu scene
    play buttons takes you to Combat Scene
    Quit buttons quits game
    
    * lowered volume

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
     public void OnPlayClicked()
     {
         Debug.Log("Play clicked!");
-        // TODO: SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("Combat");
     }
-    public void OnQuitClicked()
-    {
-        Debug.Log("Quit clicked!");
-        // TODO: Application.Quit();
-    }
+public void OnQuitClicked()
+{
+    Debug.Log("Quit clicked!");
+    
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit(); // Apparently this only works on the compiled version
+    #endif
+}
 }
